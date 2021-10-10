1183632124c39a799c513033bc56aa5cf350d037
Tyrone Oggen <oggen.tyronekyle@gmail.com>
2021-10-11 01:31:08 +0200

    Project init: Calculator UI configured and linked to VC

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Tyrone Oggen on 2021/10/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var displayLabel: UILabel!
+    
+    
+    
+    @IBAction func calcButtonPressed(_ sender: UIButton) {
+        
+        //What should happen when a non-number button is pressed
+    
+    }
+
+    
+    @IBAction func numButtonPressed(_ sender: UIButton) {
+        
+        //What should happen when a number is entered into the keypad
+    
+    }
+
+}
+
